8bd33e855b0fbd2a4941fb0593d7c44ea4dccd77
Морозов Александр <@step.kz>
2024-06-13 21:28:50 +0500

    lesson_02 files

--- bboard/urls.py
+++ bboard/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from bboard.views import index
 
 urlpatterns = [
-    path('bboard/', index),
+    path('', index),
 ]
--- bboard/views.py
+++ bboard/views.py
@@ -1,6 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
+from bboard.models import Bb
+
+
+# def index(request):
+#     template = loader.get_template('bboard/index.html')
+#     bbs = Bb.objects.order_by('-published')
+#     context = {'bbs': bbs}
+#
+#     return HttpResponse(template.render(context, request))
 
 
 def index(request):
-    return HttpResponse("Здесь будет выведен список объявлений.")
+    bbs = Bb.objects.order_by('-published')
+    context = {'bbs': bbs}
+
+    return render(request, 'bboard/index.html', context)
