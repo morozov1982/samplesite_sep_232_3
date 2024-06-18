e360e7c346ec4ce1a7538eb48dbb8ebe00fe1b1a
Морозов Александр <@step.kz>
2024-06-18 21:11:29 +0500

    lesson_03 files

--- bboard/models.py
+++ bboard/models.py
@@ -1,8 +1,32 @@
 from django.db import models
 
 
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True,
+                            verbose_name='Название')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Рубрика'
+        verbose_name_plural = 'Рубрики'
+        ordering = ['name']
+
+
 class Bb(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField(null=True, blank=True)
-    price = models.FloatField(null=True, blank=True)
-    published = models.DateTimeField(auto_now_add=True, db_index=True)
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT,
+                               verbose_name='Рубрика')
+    title = models.CharField(max_length=50, verbose_name='Товар')
+    content = models.TextField(null=True, blank=True, verbose_name='Описание')
+    price = models.FloatField(null=True, blank=True, verbose_name='Цена')
+    published = models.DateTimeField(auto_now_add=True, db_index=True,
+                                     verbose_name='Опубликовано')
+
+    def __str__(self):
+        return f'{self.title} ({self.price} тг.)'
+
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+        ordering = ['-published']
--- bboard/views.py
+++ bboard/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
-from bboard.models import Bb
+from bboard.models import Bb, Rubric
 
 
 # def index(request):
@@ -15,6 +15,17 @@ from bboard.models import Bb
 
 def index(request):
     bbs = Bb.objects.order_by('-published')
-    context = {'bbs': bbs}
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
 
     return render(request, 'bboard/index.html', context)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+
+    return render(request, 'bboard/by_rubric.html', context)
+
